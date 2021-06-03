58441f5a2f06a9c370266e9837f394f537b317c4
Marcel <MTRNord@users.noreply.github.com>
2021-06-03 12:24:52 +0200

    Update config.py

--- ffmap/config.py
+++ ffmap/config.py
@@ -25,5 +25,5 @@ CONFIG = {
 	"global_gwstat_days": 30,			# Accumulated gateway stats are collected for X days
 	"global_gwstat_show_days": 30,			# Accumulated gateway stats are shown for X days
 	"csv_dir": "/var/lib/ffmap/csv",	# Directory where the .csv files for TileStache/mapnik are stored
-	"debug_dir": "/data/fff/fffmonlog",	# Output directory for debug .txt files
+	"debug_dir": "/data/ffslfl/ffslflmonlog",	# Output directory for debug .txt files
 }
